8f41aa6e1f1556d6576d5ba20548bedf5143f3f0
Vineet Joshi <vineet0713@gmail.com>
2018-02-03 19:15:16 -0800

    Add basic UI.

--- My Favorite Movies/AppDelegate.swift
+++ My Favorite Movies/AppDelegate.swift
@@ -1,46 +1,57 @@
 //
 //  AppDelegate.swift
-//  My Favorite Movies
+//  MyFavoriteMovies
 //
-//  Created by Vineet Joshi on 1/31/18.
-//  Copyright © 2018 Vineet Joshi. All rights reserved.
+//  Created by Jarrod Parkes on 11/5/15.
+//  Copyright © 2015 Udacity. All rights reserved.
 //
 
 import UIKit
 
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // MARK: Properties
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    var sharedSession = URLSession.shared
+    var requestToken: String? = nil
+    var sessionID: String? = nil
+    var userID: Int? = nil
+    
+    // configuration for TheMovieDB, we'll take care of this for you =)...
+    var config = Config()
+    
+    // MARK: UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        // if necessary, update the configuration...
+        config.updateIfDaysSinceUpdateExceeds(7)
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+// MARK: Create URL from Parameters
+
+extension AppDelegate {
+    
+    func tmdbURLFromParameters(_ parameters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.TMDB.ApiScheme
+        components.host = Constants.TMDB.ApiHost
+        components.path = Constants.TMDB.ApiPath + (withPathExtension ?? "")
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+}
---
+++ My Favorite Movies/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  My Favorite Movies
+//
+//  Created by Vineet Joshi on 2/3/18.
+//  Copyright © 2018 Vineet Joshi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        performSegue(withIdentifier: "loginComplete", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
